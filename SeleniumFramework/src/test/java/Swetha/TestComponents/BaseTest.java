c8cac15dac889f907af1941ff5dccd0c6c3775ed
swetha659 <sjannu@IN-GPS102490.ingrnet.com>
2024-08-01 12:10:50 +0530

    Second Commit

--- SeleniumFramework/src/test/java/Swetha/TestComponents/BaseTest.java
+++ SeleniumFramework/src/test/java/Swetha/TestComponents/BaseTest.java
@@ -6,15 +6,21 @@ import java.io.IOException;
 import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.Dimension;
 import org.openqa.selenium.OutputType;
 import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.edge.EdgeOptions;
+import org.openqa.selenium.firefox.FirefoxDriver;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
@@ -22,6 +28,7 @@ import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import Swetha.pageobjects.LandingPage;
+import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class BaseTest {
 	public WebDriver driver;
@@ -39,19 +46,34 @@ public class BaseTest {
 		//String browserName = prop.getProperty("browser");
 		if(browserName.equalsIgnoreCase("chrome"))
 		{
-			driver=new ChromeDriver();
-			driver.manage().window().maximize();
+			ChromeOptions options = new ChromeOptions();
+			WebDriverManager.chromedriver().setup();
+			if(browserName.contains("headless")){
+				options.addArguments("--headless");		
+				}
+			driver=new ChromeDriver(options);
+			driver.manage().window().setSize(new Dimension(1440,900));
 			
 		}
 		else if(browserName.equalsIgnoreCase("firefox"))
 		{
+			System.setProperty("webdriver.gecko.driver",
+					"/Users/sjannu//documents//geckodriver");
+			driver = new FirefoxDriver();
 			//driver= new GeckoDriver();
 			//firefox
 		}
 		else if(browserName.equalsIgnoreCase("edge"))
 		{
 			//Edge
-			driver= new EdgeDriver();
+			EdgeOptions options = new EdgeOptions();			
+			options.addArguments("--start-maximized");
+//			options.setCapability("ms:edgeOptions", "{ \"excludeSwitches\": [\"enable-automation\"] }");
+		    //options.setExperimentalOption("excludeSwitches", List.of("disable-popup-blocking"));
+			Map<String, Object> prefs = new LinkedHashMap<>();
+			prefs.put("user_experience_metrics.personalization_data_consent_enabled", Boolean.valueOf(true));
+			options.setExperimentalOption("prefs", prefs);
+			driver = new EdgeDriver(options);
 			
 		}
 		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
